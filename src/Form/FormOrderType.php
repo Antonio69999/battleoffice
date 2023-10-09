9433e8b5f02e6b05d33d71aee23142448c7b8975
neroma71 <neroma71@gmail.com>
2023-10-09 15:40:08 +0200

    add form

---
+++ src/Form/FormOrderType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormOrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('is_paid')
+            ->add('id_client')
+            ->add('idPayment')
+            ->add('idBilingAdress')
+            ->add('idShippingAdress')
+            ->add('products')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
