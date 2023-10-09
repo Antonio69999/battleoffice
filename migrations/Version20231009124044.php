aa179eaa4bc4a17ec99b40e1d37cd7bbe2b1dbc5
neroma71 <neroma71@gmail.com>
2023-10-09 14:49:00 +0200

    add easyadmin

---
+++ migrations/Version20231009124044.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231009124044 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE adress (id INT AUTO_INCREMENT NOT NULL, adress_line VARCHAR(255) NOT NULL, zipcode VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, adress_line2 VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, country VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, id_client_id INT DEFAULT NULL, id_payment_id INT DEFAULT NULL, id_biling_adress_id INT DEFAULT NULL, id_shipping_adress_id INT DEFAULT NULL, is_paid TINYINT(1) NOT NULL, INDEX IDX_F529939899DED506 (id_client_id), UNIQUE INDEX UNIQ_F5299398A149236C (id_payment_id), INDEX IDX_F529939838CE2634 (id_biling_adress_id), INDEX IDX_F529939841511A34 (id_shipping_adress_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE payment (id INT AUTO_INCREMENT NOT NULL, method VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product_order (product_id INT NOT NULL, order_id INT NOT NULL, INDEX IDX_5475E8C44584665A (product_id), INDEX IDX_5475E8C48D9F6D38 (order_id), PRIMARY KEY(product_id, order_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F529939899DED506 FOREIGN KEY (id_client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A149236C FOREIGN KEY (id_payment_id) REFERENCES payment (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F529939838CE2634 FOREIGN KEY (id_biling_adress_id) REFERENCES adress (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F529939841511A34 FOREIGN KEY (id_shipping_adress_id) REFERENCES adress (id)');
+        $this->addSql('ALTER TABLE product_order ADD CONSTRAINT FK_5475E8C44584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE product_order ADD CONSTRAINT FK_5475E8C48D9F6D38 FOREIGN KEY (order_id) REFERENCES `order` (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939899DED506');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A149236C');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939838CE2634');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939841511A34');
+        $this->addSql('ALTER TABLE product_order DROP FOREIGN KEY FK_5475E8C44584665A');
+        $this->addSql('ALTER TABLE product_order DROP FOREIGN KEY FK_5475E8C48D9F6D38');
+        $this->addSql('DROP TABLE adress');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE country');
+        $this->addSql('DROP TABLE `order`');
+        $this->addSql('DROP TABLE payment');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE product_order');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
