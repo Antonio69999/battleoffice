9697ac6809dadb853451d709c8b743c0fa09412b
Antonio69999 <antoine.chabeauti@gmail.com>
2023-10-09 14:38:40 +0200

    entity

---
+++ src/Entity/Order.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $is_paid = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    private ?Client $id_client = null;
+
+    #[ORM\OneToOne(inversedBy: 'orders', cascade: ['persist', 'remove'])]
+    private ?Payment $idPayment = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orders')]
+    private ?Adress $idBilingAdress = null;
+
+    #[ORM\ManyToOne(inversedBy: 'ShippingOrders')]
+    private ?Adress $idShippingAdress = null;
+
+    #[ORM\ManyToMany(targetEntity: Product::class, mappedBy: 'product')]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isIsPaid(): ?bool
+    {
+        return $this->is_paid;
+    }
+
+    public function setIsPaid(bool $is_paid): static
+    {
+        $this->is_paid = $is_paid;
+
+        return $this;
+    }
+
+    public function getIdClient(): ?Client
+    {
+        return $this->id_client;
+    }
+
+    public function setIdClient(?Client $id_client): static
+    {
+        $this->id_client = $id_client;
+
+        return $this;
+    }
+
+    public function getIdPayment(): ?Payment
+    {
+        return $this->idPayment;
+    }
+
+    public function setIdPayment(?Payment $idPayment): static
+    {
+        $this->idPayment = $idPayment;
+
+        return $this;
+    }
+
+    public function getIdBilingAdress(): ?Adress
+    {
+        return $this->idBilingAdress;
+    }
+
+    public function setIdBilingAdress(?Adress $idBilingAdress): static
+    {
+        $this->idBilingAdress = $idBilingAdress;
+
+        return $this;
+    }
+
+    public function getIdShippingAdress(): ?Adress
+    {
+        return $this->idShippingAdress;
+    }
+
+    public function setIdShippingAdress(?Adress $idShippingAdress): static
+    {
+        $this->idShippingAdress = $idShippingAdress;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->addProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        if ($this->products->removeElement($product)) {
+            $product->removeProduct($this);
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    #[ORM\ManyToMany(targetEntity: order::class, inversedBy: 'products')]
+    private Collection $product;
+
+    public function __construct()
+    {
+        $this->product = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, order>
+     */
+    public function getProduct(): Collection
+    {
+        return $this->product;
+    }
+
+    public function addProduct(order $product): static
+    {
+        if (!$this->product->contains($product)) {
+            $this->product->add($product);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(order $product): static
+    {
+        $this->product->removeElement($product);
+
+        return $this;
+    }
+}
