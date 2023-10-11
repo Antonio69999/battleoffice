ef3b0163dde530a88010e030809477fb8464332c
neroma71 <neroma71@gmail.com>
2023-10-11 14:49:04 +0200

    add psuh

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,9 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Product;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
