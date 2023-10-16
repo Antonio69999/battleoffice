d9a02522e256ca8911a6fb6a312056f567073861
Antonio69999 <antoine.chabeauti@gmail.com>
2023-10-16 16:07:50 +0200

    yenamarre

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -25,23 +25,6 @@ class LandingPageController extends AbstractController
 
         $form->handleRequest($request);
 
-        //GUZZLE
-
-        $client = new Client([
-            'base_uri' => 'http://httpbin.org',
-            'timeout' => 2.0,
-        ]);
-
-        //$jsonData = ['order' => $jsonOrder];
-        
-        $token = "mJxTXVXMfRzLg6ZdhUhM4F6Eutcm1ZiPk4fNmvBMxyNR4ciRsc8v0hOmlzA0vTaX";
-
-        $header = [
-            'Authorization' => "Bearer" . $token,
-        ];
-
-        // $response = $client->request('POST', 'test');
-
         if ($form->isSubmitted() && $form->isValid()) {
 
             $selectedProductID = $request->request->get('selected_product_id');
@@ -51,63 +34,46 @@ class LandingPageController extends AbstractController
                 $products = $productRepository->find($selectedProductID);
 
 
-            $paymentMethod = $request->request->get('payment');
-            $payment = $paymentRepository->findOneBy(['method' => $paymentMethod]);
-            
-            $jsonOrder = [
-                'status' => $order->getStatus(),
-                'client' => [
-                    'firstname' => $order->getClient()->getFirstname(),
-                    'lastname' => $order->getClient()->getLastname(),
-                    'email' => $order->getClient()->getEmail(),
-                ],
-                'addresses' => [
-                    'billing' => [
-                        'address_line1' => $order->getBilingAdress()->getAdressLine(),
-                        'address_line2' => $order->getBilingAdress()->getAdressLine2(),
-                        'city' => $order->getBilingAdress()->getCity(),
-                        'zipcode' => $order->getBilingAdress()->getZipcode(),
-                        'country' => $order->getBilingAdress()->getCountry()->getCountry(),
-                        'phone' => $order->getBilingAdress()->getPhone(),
-                    ],
-                    'shipping' => [
-                        'address_line1' => $order->getShippingAdress()->getAdressLine(),
-                        'address_line2' => $order->getShippingAdress()->getAdressLine2(),
-                        'city' => $order->getShippingAdress()->getCity(),
-                        'zipcode' => $order->getShippingAdress()->getZipcode(),
-                        'country' => $order->getShippingAdress()->getCountry()->getCountry(),
-                        'phone' => $order->getShippingAdress()->getPhone(),
-                    ],
-                ],
-                'payment_method' => [
-                    'method' => $order->getPayment() ? $order->getPayment()->getMethod() : null,
-                ],
-                'products' => $products->getName(), 
-            ];
-           
-            $jsonOrder = json_encode(['order' => $jsonOrder]);
+                $paymentMethod = $request->request->get('payment');
+                $payment = $paymentRepository->findOneBy(['method' => $paymentMethod]);
 
-            $client = new Client([
-                'base_uri' => 'https://api-commerce.simplon-roanne.com/',
-                'timeout'  => 2.0,
-            ]);
+                $jsonOrder = [
+                    'status' => $order->getStatus(),
+                    'client' => [
+                        'firstname' => $order->getClient()->getFirstname(),
+                        'lastname' => $order->getClient()->getLastname(),
+                        'email' => $order->getClient()->getEmail(),
+                    ],
+                    'addresses' => [
+                        'billing' => [
+                            'address_line1' => $order->getBilingAdress()->getAdressLine(),
+                            'address_line2' => $order->getBilingAdress()->getAdressLine2(),
+                            'city' => $order->getBilingAdress()->getCity(),
+                            'zipcode' => $order->getBilingAdress()->getZipcode(),
+                            'country' => $order->getBilingAdress()->getCountry()->getCountry(),
+                            'phone' => $order->getBilingAdress()->getPhone(),
+                        ],
+                        'shipping' => [
+                            'address_line1' => $order->getShippingAdress()->getAdressLine(),
+                            'address_line2' => $order->getShippingAdress()->getAdressLine2(),
+                            'city' => $order->getShippingAdress()->getCity(),
+                            'zipcode' => $order->getShippingAdress()->getZipcode(),
+                            'country' => $order->getShippingAdress()->getCountry()->getCountry(),
+                            'phone' => $order->getShippingAdress()->getPhone(),
+                        ],
+                    ],
+                    'payment_method' => [
+                        'method' => $order->getPayment() ? $order->getPayment()->getMethod() : null,
+                    ],
+                    'products' => $products->getName(),
+                ];
 
-            
-            // les methodes qui set
-            $order->addProduct($products);
-            $order->setPayment($payment);
-            $order->setStatus('WAITING');
-         
-            //PERSIST
-            // $entityManager->persist($client);
-            $entityManager->persist($order);  
-            $entityManager->flush();
+                $jsonOrder = json_encode(['order' => $jsonOrder]);
 
                 $client = new Client([
                     'base_uri' => 'https://api-commerce.simplon-roanne.com/',
                     'timeout'  => 2.0,
                 ]);
-
 
                 // les methodes qui set
                 $order->addProduct($products);
